4535c0674351ab91f37e7cd5389c39dad1ba79f0
joedietrich-dev <joe@joedietrich.net>
2021-11-06 14:58:55 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.start_with?(/[A-Z]/) && self.end_with?(".")
   end
 
   def question?
-
+    self.start_with?(/[A-Z]/) && self.end_with?("?")
   end
 
   def exclamation?
-
+    self.start_with?(/[A-Z]/) && self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[\.\?\!]+\s?/).count
   end
 end
